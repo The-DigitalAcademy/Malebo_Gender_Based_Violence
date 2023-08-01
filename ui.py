a39f762db5511a67fbcf414de6ed5e1cd483299d
Malebo16 <Malebo.Phosa@dadevs.co.za>
2023-08-01 16:03:39 +0200

    training model

---
+++ ui.py
@@ -0,0 +1,56 @@
+import streamlit as st
+import pickle
+
+#Function to load the selected model
+def load_model(model_name):
+    model_path = f'/Users/da_m1_46/Desktop/Malebo_Customer_Income_Prediction/{model_name}.pkl'
+    with open(model_path, 'rb') as file:
+        model = pickle.load(file)
+    return model
+
+def main():
+  # Title of the web app
+    st.title('Gender Based Violance')
+
+    # Set custom CSS for the background image
+    page_bg_img = '''
+    <style>
+    .stApp {
+        background-image: url('image.png');
+        background-size: cover;
+    }
+    </style>
+    '''
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    
+    
+    # Subheader
+    st.subheader('Welcome! Select a model and input features for prediction.')
+
+    # Dropdown to select the model
+    model_options = ['RandomForest',  'KNeighbors', 'LogisticRegression']
+    selected_model = st.selectbox('Select Model', model_options)  
+
+    # Load the selected model
+    model = load_model(selected_model)
+
+    # User input for features
+    st.header('Feature Input')
+    feature1 = st.number_input('tweet')
+
+
+
+    # Button for predictions
+    clicked = st.button('Get Predictions')
+
+    # Perform predictions when the button is clicked
+    if clicked:
+        # Perform predictions using the selected model
+        prediction = model.predict([[feature1]])
+
+        # Display the prediction result
+        st.header('Prediction')
+        st.write(f'The prediction result is: R {prediction[0]}')
+
+if __name__ == '__main__':
+    main()
